6226f3ced66bab7984e0edacf8d36778c12dd625
stefffgualtieri <s.gualtieri5@campus.unimib.it>
2025-08-22 01:53:54 +0200

    Modified temporal fitness using Pytorch

--- temporal_fitness.py
+++ temporal_fitness.py
@@ -1,4 +1,37 @@
 import numpy as np
+import torch
+'''
+Input:
+- spike_times (torch.Tensor): shape (n_samples, num_classes), spike times of output neurons.
+- target_classes (torch.Tensor): shape (n_samples,), true class indices.
+- num_classes (int): number of output neurons/classes.
+- t_max (int): maximum simulation time.
+- tau (int): desired firing time for target neuron.
+- device (str): 'cpu' or 'cuda'.
+
+Returns:
+- torch.Tensor: scalar, mean temporal error across samples
+'''
+
+
+def temporal_fitness_function(spike_times, target_classes, num_classes, t_max=255, tau=20, device="cpu"):
+
+    n_samples = spike_times.shape[0]
+
+    #Copstruisci target firing times: default t_max
+    target_firing_times = torch.full((n_samples, num_classes), t_max, device=device)
+
+    #Tau for the neurons of the correct class
+    target_firing_times[torch.arange(n_samples), target_classes] = tau
+
+    #calcola gli errori normalizzati
+    errors = (spike_times - target_firing_times)/t_max
+
+    #Errore quadrato per ogni campione
+    squared_error = 0.5 * torch.sum(errors ** 2, dim = 1)
+
+    return squared_error.mean()
+
 
 """
 Input:
@@ -16,20 +49,20 @@ Returns:
     The total error of the dataset
 """
 
-def temporal_fitness_function(spike_times, target_classes, num_classes, t_max=255, tau=20):
+# def temporal_fitness_function(spike_times, target_classes, num_classes, t_max=255, tau=20):
 
-    n_samples = spike_times.shape[0]
-    total_error = 0.0
+#     n_samples = spike_times.shape[0]
+#     total_error = 0.0
 
-    for i in range(n_samples):
+#     for i in range(n_samples):
 
-        #create a numpy array of lenght num_classes where each element is equal to t_max
-        target_firing_times = np.full((num_classes,), t_max)
-        target_firing_times[target_classes[i]] = tau
+#         #create a numpy array of lenght num_classes where each element is equal to t_max
+#         target_firing_times = np.full((num_classes,), t_max)
+#         target_firing_times[target_classes[i]] = tau
         
-        #calculate the error of this sample and add to the total error
-        errors = (spike_times[i] - target_firing_times) / t_max
-        squared_error = 0.5 * np.sum(errors ** 2)
-        total_error += squared_error
+#         #calculate the error of this sample and add to the total error
+#         errors = (spike_times[i] - target_firing_times) / t_max
+#         squared_error = 0.5 * np.sum(errors ** 2)
+#         total_error += squared_error
 
-    return total_error / n_samples
+#     return total_error / n_samples
